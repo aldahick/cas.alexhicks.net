928fe76a8067685d84dc408fe239c6b4cd7f8cf2
Alex Hicks <aldahick@iupui.edu>
2016-05-18 00:27:56 -0400

    Fix MySQL not connecting

--- inc/mysql.php
+++ inc/mysql.php
@@ -3,8 +3,8 @@ require_once(__DIR__ . "/config.php");
 require_once(__DIR__ . "/../php-utils/Database.php");
 
 function setup_database() {
-	global $MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DATABASE;
-	$db = new Database($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DATABASE);
+	global $MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DATABASE, $MYSQL_PORT;
+	$db = new Database($MYSQL_HOSTNAME, $MYSQL_USERNAME, $MYSQL_PASSWORD, $MYSQL_DATABASE, $MYSQL_PORT);
 	if ($db->is_dead) {
 		return false;
 	}
